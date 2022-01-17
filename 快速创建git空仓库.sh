a321aea41ecb8e02535debdd73ee7d6c69025403
HuangYi <123337671@qq.com>
2022-01-18 04:45:45 +0800

    update

--- 快速创建git空仓库.sh
+++ 快速创建git空仓库.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # 功能：在 ~/APP 下创建一个 blog 文件夹存放空的git仓库
 # 执行脚本的用户最好是 ubuntu (非root)
+# 添加远程仓库示例: git remote add origin ubuntu@9cats.link:~/APP/www/blog/.git
 
-dirName="blog"
+name="blog"
 
 cd ~ #先切换到用户目录目录
 
@@ -11,14 +12,19 @@ if [ ! -d "APP" ]; then
   mkdir APP
 fi
 
+# 判断是否有www文件夹
+if [ ! -d "APP/www" ]; then
+  mkdir APP/www
+fi
+
 # 判断是否有blog文件夹，没有再创建该仓库
-if [ ! -d "APP/${dirName}" ]; then
-  mkdir APP/${dirName} && cd $_ # 创建文件夹并进入
-  git init --bare ./${dirName}.git  # 创建空的仓库
+if [ ! -d "APP/www/${name}" ]; then
+  mkdir APP/www/${name} && cd $_ # 创建文件夹并进入
+  git init --bare .git  # 创建空的仓库
   # 写入钩子函数
-  echo '#!/bin/bash'                                               > ${dirName}.git/hooks/post-receive #声明类型
-  echo 'cd ..'                                                    >> ${dirName}.git/hooks/post-receive #先返回上一级
-  echo "git --work-tree=. --git-dir=./${dirName}.git checkout -f" >> ${dirName}.git/hooks/post-receive #签出
+  echo '#!/bin/bash'                                   > .git/hooks/post-receive #声明类型
+  echo 'cd ..'                                        >> .git/hooks/post-receive #先返回上一级
+  echo "git --work-tree=. --git-dir=.git checkout -f" >> .git/hooks/post-receive #签出
   
-  chmod +x ${dirName}.git/hooks/post-receive # 赋予可执行权限
+  chmod +x .git/hooks/post-receive # 赋予可执行权限
 fi
